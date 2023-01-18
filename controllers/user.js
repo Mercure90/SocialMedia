310f2f0745a9fa0ed5762894df87f6189eae8941
mpastore90 <mpastore90@users.noreply.github.com>
2023-01-18 13:00:32 +0100

    Forbidden Error + follow user

--- controllers/user.js
+++ controllers/user.js
@@ -1,16 +1,20 @@
 const User = require("../models/User");
 const { StatusCodes } = require("http-status-codes");
-const { BadRequestError, UnauthenticatedError } = require("../errors");
+const {
+  BadRequestError,
+  UnauthenticatedError,
+  ForbidenError,
+} = require("../errors");
 
 const jwt = require("jsonwebtoken");
 
 const getUser = async (req, res) => {
-    const user = await User.findOne({_id: req.params.id})
-    if (!user) {
-        throw new NotFoundError(`No user with id ${req.params.id}`)
-    }
-    const {password,updatedAt, ...others} =user._doc
-    res.status(200).json(others);
+  const user = await User.findOne({ _id: req.params.id });
+  if (!user) {
+    throw new NotFoundError(`No user with id ${req.params.id}`);
+  }
+  const { password, updatedAt, ...others } = user._doc;
+  res.status(200).json(others);
 };
 const updateUser = async (req, res) => {
   if (req.body.userId === req.params.id || req.body.isAdmin) {
@@ -19,30 +23,42 @@ const updateUser = async (req, res) => {
     });
     return res.status(200).json("Account has been updated");
   } else {
-    return res.status(403).json("you can only update your own accout !");
+    throw new ForbidenError("you can only update your own accout !");
   }
 };
 const deleteUser = async (req, res) => {
   if (req.body.userId === req.params.id || req.body.isAdmin) {
-    const user = await User.deleteOne({_id: req.params.id});
+    const user = await User.deleteOne({ _id: req.params.id });
     return res.status(200).json("Account has been deleted");
   } else {
-    return res.status(403).json("you can only delete your own accout !");
+    throw new ForbidenError("you can only delete your own accout !");
   }
 };
 
-const followUser = async (req,res) =>{
-  return res.status(200).json("follow user");
-}
+const followUser = async (req, res) => {
+  if (req.body.userId !== req.params.id) {
+    const user = await User.findById(req.params.id);
+    const currentUser = await User.findById(req.body.userId);
+    if (!user.followers.includes(req.body.userId)) {
+      await user.updateOne({ $push: { followers: req.body.userId } });
+      await currentUser.updateOne({ $push: { following: req.params.id } });
+      res.status(200).json("user has been followed")
+    } else {
+      throw new ForbidenError("You already follow this user");
+    }
+    return res.status(200).json("follow user");
+  }
+  throw new ForbidenError("you can't follow yourself' !");
+};
 
-const unfollowUser = async (req,res) =>{
+const unfollowUser = async (req, res) => {
   return res.status(200).json("unfollow user");
-}
+};
 
 module.exports = {
   updateUser,
   getUser,
   deleteUser,
   followUser,
-  unfollowUser
+  unfollowUser,
 };
--- errors/index.js
+++ errors/index.js
@@ -2,10 +2,12 @@ const CustomAPIError = require('./custom-api')
 const UnauthenticatedError = require('./unauthenticated')
 const NotFoundError = require('./not-found')
 const BadRequestError = require('./bad-request')
+const ForbidenError = require('./forbiden.js')
 
 module.exports = {
   CustomAPIError,
   UnauthenticatedError,
-  NotFoundError,
+  NotFoundError, 
   BadRequestError,
+  ForbidenError
 }
